3af68b6b5914f6eea911603079dc008aa9ea2cfb
donghyuk <dhlovey@gmail.com>
2023-02-04 00:51:31 +0900

    쿠키허용

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,9 +3,18 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
 
 class VerifyCsrfToken extends Middleware
 {
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('P3P', 'CP="IDC DSP COR ADM DEVi TATi PSA PSD IVAi IVDi CONi HIS OUR IND CNT');
+        return $response;
+    }
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
